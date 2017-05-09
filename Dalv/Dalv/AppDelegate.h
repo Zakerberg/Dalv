847c16e38055a5902b1ac2314dba892cacd11e13
Zakerberg <blmichael1129@icloud.com>
2017-05-09 11:51:35 +0800

    创建项目 -- >  DalvApp

---
+++ Dalv/Dalv/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dalv
+//
+//  Created by Michael 柏 on 2017/5/9.
+//  Copyright © 2017年 Michael 柏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
