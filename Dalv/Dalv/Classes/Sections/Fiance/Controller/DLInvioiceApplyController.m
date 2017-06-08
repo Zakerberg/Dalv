17033a01e50875268fb59ffdc5deb27d91f65ecc
Zakerberg <blmichael1129@icloud.com>
2017-06-08 18:02:48 +0800

    发票,

--- Dalv/Dalv/Classes/Sections/Fiance/Controller/DLInvioiceApplyController.m
+++ Dalv/Dalv/Classes/Sections/Fiance/Controller/DLInvioiceApplyController.m
@@ -7,10 +7,17 @@
 //   ---------------   发票申请   ------------
 
 #import "DLInvioiceApplyController.h"
+#import "DLHomeViewTask.h"
 
 @interface DLInvioiceApplyController ()<UITableViewDelegate,UITableViewDataSource,UITextViewDelegate>
-
 @property (nonatomic, strong) UITableView *invoiceTableView;
+
+/*** 发票抬头TF  ***/
+@property(nonatomic,strong) UITextField * companyTF;
+/*** 发票项目TF  ***/
+@property(nonatomic,strong) UITextField *projectTF;
+/*** 发票备注TF  ***/
+@property(nonatomic,strong) UITextView *noteTextView;
 
 @end
 
@@ -23,32 +30,24 @@ static NSString *cell2ID = @"cell2ID";
     [self setTableView];
     self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
     self.title = @"发票申请";
-   
 }
-
-
 
 #pragma mark ----------------- Set TableView -----------------
 
 -(void)setTableView {
     
     self.view.backgroundColor = [UIColor ms_backgroundColor];
-    self.invoiceTableView = [[UITableView alloc]initWithFrame:CGRectZero style:UITableViewStylePlain];
+    self.invoiceTableView = [[UITableView alloc]initWithFrame:CGRectZero style:UITableViewStyleGrouped];
     self.invoiceTableView.dataSource = self;
     self.invoiceTableView.backgroundColor = [UIColor ms_backgroundColor];
     self.invoiceTableView.delegate = self;
-    [self.invoiceTableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+//    [self.invoiceTableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     self.automaticallyAdjustsScrollViewInsets = NO;
     self.invoiceTableView.showsVerticalScrollIndicator = NO;
-
+    
     [self.invoiceTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellID];
     
     [self.invoiceTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cell2ID];
-    
-
-    
-    
-    
     
     [self.view addSubview:self.invoiceTableView];
     
@@ -70,7 +69,6 @@ static NSString *cell2ID = @"cell2ID";
     // Dispose of any resources that can be recreated.
 }
 
-
 #pragma mark  ----------UITable View Delegate------------
 
 -(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
@@ -79,22 +77,29 @@ static NSString *cell2ID = @"cell2ID";
         return 0.1;
     }
     
-    return 25;
+    else if (section == 1){
+        return 14;
+    }
+    else if (section == 4){
+        return 10;
+    }
+    
+    return 32;
 }
 
-
-
-
-
-
-
-
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-
+    
     if (indexPath.section == 0 && indexPath.row == 2) {
         return 75;
     }
-
+    else if (indexPath.section == 0 && indexPath.row == 3){
+        return 32;
+    }
+    
+    else if (indexPath.section == 4 && indexPath.row == 0){
+        return 70;
+    }
+    
     return 45;
 }
 
@@ -102,64 +107,186 @@ static NSString *cell2ID = @"cell2ID";
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     
     return 6;
-    
 }
 
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    
+    if (indexPath.section == 0 && indexPath.row == 1) {
+        //来个选择器 ----  ---- --- -- - - -- - -- - - -- -  - - - - -
+    }
+    
+    
+    
 }
-
-
 
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-
     if (section == 0) {
         return 4;
     }
+    else if (section == 1 || section == 2 || section == 3 || section == 5){
+        return 1;
+    }
     
-    return 2;
-    
-    
+    else if (section == 4){
+        return 3;
+    }
+
+    return 0;
 }
-
-
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (indexPath.section == 0){
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
 
-    return cell;
+        if (indexPath.row == 0) {
+            UILabel *companyLabel = [[UILabel alloc]init];
+            companyLabel.text = @"发票抬头";
+            companyLabel.font = [UIFont systemFontOfSize:16];
+            companyLabel.textColor = [UIColor colorWithHexString:@"#494949"];
+            [companyLabel sizeToFit];
+            [self.invoiceTableView addSubview:companyLabel];
+            
+            
+            UITextField *companyTF = [[UITextField alloc] init];
+            self.companyTF = companyTF;
+            companyTF.placeholder = @"请输入公司名称";
+            companyTF.borderStyle = UITextBorderStyleNone;
+            
+            [self.invoiceTableView addSubview:companyTF];
+            
+            [companyLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+                make.left.top.height.equalTo(@15);
+            }];
+            
+            [companyTF mas_makeConstraints:^(MASConstraintMaker *make) {
+               make.centerY.equalTo(companyLabel.mas_centerY).offset(0);
+                make.height.offset(44);
+                make.right.offset(0);
+                make.left.equalTo(companyLabel.mas_right).offset(15);
+            }];
+
+        }else if (indexPath.row == 1){
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            
+            UILabel *projectLabel = [[UILabel alloc]init];
+            projectLabel.text = @"发票项目";
+            projectLabel.font = [UIFont systemFontOfSize:16];
+            projectLabel.textColor = [UIColor colorWithHexString:@"#494949"];
+            [projectLabel sizeToFit];
+            [self.invoiceTableView addSubview:projectLabel];
+
+            UITextField *projectTF = [[UITextField alloc] init];
+            self.projectTF = projectTF;
+            projectTF.placeholder = @"请选择发票项目,团款";
+            projectTF.borderStyle = UITextBorderStyleNone;
+            
+            [self.invoiceTableView addSubview:projectTF];
+
+            
+            [projectLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+                make.left.height.equalTo(@15);
+                make.top.equalTo(@60);
+            }];
+            
+            [projectTF mas_makeConstraints:^(MASConstraintMaker *make) {
+                make.centerY.equalTo(projectLabel.mas_centerY).offset(0);
+                make.height.offset(44);
+                make.right.offset(0);
+                make.left.equalTo(projectLabel.mas_right).offset(15);
+            }];
+        }
+        /** 发票备注 **/
+        else if (indexPath.row == 2){
+            
+            UILabel *noteLabel = [[UILabel alloc]init];
+            
+            noteLabel.text = @"发票备注";
+            noteLabel.font = [UIFont systemFontOfSize:16];
+            noteLabel.textColor = [UIColor colorWithHexString:@"#494949"];
+            [noteLabel sizeToFit];
+            
+            [self.invoiceTableView addSubview:noteLabel];
+            
+            UITextView *noteTextView = [[UITextView alloc] init];
+            self.noteTextView = noteTextView;
+            
+            noteTextView.font = [UIFont systemFontOfSize:15];
+            noteTextView.delegate = self;
+            noteTextView.textColor = [UIColor colorWithHexString:@"#c1c1c1"];
+            noteTextView.text = @"请填写备注 如代订机票,代订房费等";
+
+            [self.invoiceTableView addSubview:noteTextView];
+            
+            [noteLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+                make.left.height.equalTo(@15);
+                make.top.equalTo(@105);
+            }];
+            
+            [noteTextView mas_makeConstraints:^(MASConstraintMaker *make) {
+                make.height.offset(75);
+                make.left.equalTo(noteLabel.mas_right).offset(15);
+//                make.width.offset(0);
+                make.top.offset(90);
+            }];
+        }
+        
+        else if (indexPath.row == 3){
+            UILabel *amountLabel = [[UILabel alloc]init];
+            amountLabel.text = @"剩余发票额度:";
+            amountLabel.font = [UIFont systemFontOfSize:16];
+            amountLabel.textColor = [UIColor colorWithHexString:@"#494949"];
+            [amountLabel sizeToFit];
+            
+            [self.invoiceTableView addSubview:amountLabel];
+
+            UILabel *moneyLabel = [[UILabel alloc] init];
+            moneyLabel.text = @"XXXXX元";
+            moneyLabel.font = [UIFont systemFontOfSize:16];
+            moneyLabel.textColor = [UIColor colorWithHexString:@"#ff623d"];
+            [moneyLabel sizeToFit];
+            
+            [self.invoiceTableView addSubview:moneyLabel];
+
+            
+            [amountLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+//               make
+            }];
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+            
+            
+            
+            
+            
+        
+        return cell;
     }
-    
-    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cell2ID];
     
     return cell;
-
-
 }
-
-
-
-
-
-
-
-
 
 #pragma mark ---------  UITextViewDelegate  -------------
 
 -(void)textViewDidBeginEditing:(UITextView *)textView{
-    if ([textView.text isEqualToString:@"请填写您的收货地址"]) {
+    if ([textView.text isEqualToString:@"请填写备注 如代订机票,代订房费等"]) {
         textView.text = @"";
-        textView.textColor = [UIColor colorWithHexString:@"b6b6b6"];
+        textView.textColor = [UIColor colorWithHexString:@"c1c1c1"];
     }
 }
-
 
 -(void)textViewDidEndEditing:(UITextView *)textView{
     
--- Dalv/Dalv/Classes/Sections/Mine/Controller/DLMyCustomerController.m
+++ Dalv/Dalv/Classes/Sections/Mine/Controller/DLMyCustomerController.m
@@ -8,15 +8,25 @@
 
 #import "DLMyCustomerController.h"
 
-@interface DLMyCustomerController ()
+@interface DLMyCustomerController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property(nonatomic,strong) UITableView * customerTableView;
 
 @end
 
+static NSString *cellID = @"cellID";
 @implementation DLMyCustomerController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setTableView];
+    self.title = @"我的直客";
+    self.view.backgroundColor = [UIColor ms_backgroundColor];;
+}
+
+- (BOOL)dl_blueNavbar {
+    return YES;
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +34,105 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark -----------   Set UITableView ----------------
+
+-(void)setTableView {
+    
+    self.customerTableView = [[UITableView alloc]initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+    self.customerTableView.dataSource = self;
+    self.customerTableView.backgroundColor = [UIColor ms_backgroundColor];
+    self.customerTableView.delegate = self;
+    [self.customerTableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.customerTableView.showsVerticalScrollIndicator = NO;
+    
+    [self.customerTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellID];
+    
+
+    
 }
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#pragma mark ----------- UITable View Delegate ----------------
+
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    
+    if (section == 0) {
+        return 0.1;
+    }
+    
+    return 25;
+}
+
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 45;
+}
+
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    
+    return 2;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (section == 0) {
+        return 1;
+    }
+    return 3;
+}
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    return cell;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
