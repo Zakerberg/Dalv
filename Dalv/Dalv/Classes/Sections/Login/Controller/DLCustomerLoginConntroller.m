0efff19b4a65b6a64d1dbca02c4213f0236a6e47
Zakerberg <blmichael1129@icloud.com>
2017-06-21 11:57:10 +0800

    增加接口

---
+++ Dalv/Dalv/Classes/Sections/Login/Controller/DLCustomerLoginConntroller.m
@@ -0,0 +1,56 @@
+//
+//  DLCustomerLoginConntroller.m
+//  Dalv
+//
+//  Created by Michael 柏 on 2017/6/21.
+//  Copyright © 2017年 Michael 柏. All rights reserved.
+//   ------------------  顾客登录   -------------------
+
+#import "DLCustomerLoginConntroller.h"
+#import "DLHomeViewTask.h"
+
+@interface DLCustomerLoginConntroller ()
+
+@end
+
+@implementation DLCustomerLoginConntroller
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self fetchData];
+    [self setUI];
+
+}
+
+-(void)setUI{
+    
+    
+    
+}
+
+
+-(void)fetchData{
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
