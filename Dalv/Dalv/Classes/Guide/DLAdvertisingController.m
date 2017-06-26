50509ebfe424590556cfcb54fe8e73524e700dca
Zakerberg <blmichael1129@icloud.com>
2017-06-26 19:59:47 +0800

    小改

--- Dalv/Dalv/Classes/Guide/DLAdvertisingController.m
+++ Dalv/Dalv/Classes/Guide/DLAdvertisingController.m
@@ -42,42 +42,39 @@
         iv.frame = CGRectMake(i * scrollView.frame.size.width, 0, scrollView.frame.size.width, scrollView.frame.size.height);
         [scrollView addSubview:iv];
     }
-    //设置内容的大小
-    //因为不需要竖向滚动，所以高度只要小于scrollview的高度就可以
     scrollView.contentSize = CGSizeMake(scrollView.frame.size.width * imageNames.count, 0);
     
     scrollView.pagingEnabled = YES;
     
-    //  添加页数控制视图 new = alloc + init
-//    UIPageControl *pageControl = [UIPageControl new
-//                                  ];
-    //不要加到滚动视图中， 会随着滚动消失掉
-//    [self.view addSubview:pageControl];
-    //    设置常用属性,距离屏幕下方60像素。
-//    pageControl.frame = CGRectMake(0, self.view.frame.size.height - 60, self.view.frame.size.width, 30);
-    //    设置圆点的个数
-//    pageControl.numberOfPages = imageNames.count;
-    //    设置没有被选中时圆点的颜色
-//    pageControl.pageIndicatorTintColor = [UIColor blackColor];
-//        设置选中时圆点的颜色
-//    pageControl.currentPageIndicatorTintColor = [UIColor redColor];
-    //    关闭分页控件的用户交互功能
-//    pageControl.userInteractionEnabled = NO;
-    
-    // 为了检测滚动视图的偏移量，引入代理
+/*
+   添加页数控制视图 new = alloc + init
+     UIPageControl *pageControl = [UIPageControl new
+                                   ];
+ 不要加到滚动视图中， 会随着滚动消失掉
+     [self.view addSubview:pageControl];
+     设置常用属性,距离屏幕下方60像素。
+     pageControl.frame = CGRectMake(0, self.view.frame.size.height - 60, self.view.frame.size.width, 30);
+     设置圆点的个数
+     pageControl.numberOfPages = imageNames.count;
+     设置没有被选中时圆点的颜色
+     pageControl.pageIndicatorTintColor = [UIColor blackColor];
+         设置选中时圆点的颜色
+     pageControl.currentPageIndicatorTintColor = [UIColor redColor];
+     关闭分页控件的用户交互功能
+     pageControl.userInteractionEnabled = NO;
+ */
+
     scrollView.delegate = self;
     
 //    pageControl.tag = 1000;
-//    为最后一页添加按钮
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
     [btn setTitle:@"点击进入" forState:UIControlStateNormal];
     //因为是滚动视图最后一页，所以要添加到滚动视图中
     [scrollView addSubview:btn];
-    btn.frame = CGRectMake(0, 0, 100, 40);
-    //把按钮添加到第四页的中心
-    btn.center = CGPointMake((imageNames.count - 0.5) * scrollView.frame.size.width, scrollView.frame.size.height - 100);
-    [btn addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
     
+    btn.frame = CGRectMake(0, 0, 100, 44);
+    btn.center = CGPointMake((imageNames.count - 0.5) * scrollView.frame.size.width - 100, scrollView.frame.size.height - 100);
+    [btn addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)click{
@@ -89,7 +86,7 @@
 
 #pragma mark - - UIScrollViewDelegate --
 
-//当滚动视图发生位移，就会进入下方代理方法
+
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView{
     UIPageControl *pageControl = (UIPageControl *)[self.view viewWithTag:1000];
     //取得偏移量
@@ -97,7 +94,7 @@
     //根据滚动的位置来决定当前是第几页
     //可以用 round()  C语言方法进行 四舍五入操作
     NSInteger index = round(point.x/scrollView.frame.size.width);
-    //设置分页控制器的当前页面
+
     pageControl.currentPage = index;
 }
 
