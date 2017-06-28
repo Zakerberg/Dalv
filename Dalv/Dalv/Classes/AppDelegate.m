b984cb1a45d696aa44e42046a1dfd875d3653593
Zakerberg <blmichael1129@icloud.com>
2017-06-28 14:57:32 +0800

    小改

--- Dalv/Dalv/Classes/AppDelegate.m
+++ Dalv/Dalv/Classes/AppDelegate.m
@@ -56,12 +56,17 @@
 
 #pragma mark - setUp IQKeyboardManager
 - (void)setUpKeyboardManager {
-    IQKeyboardManager *manager = [IQKeyboardManager sharedManager];
-    manager.enable = NO;//控制整个功能是否启用。
-    manager.shouldResignOnTouchOutside = YES;//控制点击背景是否收起键盘
-    manager.shouldToolbarUsesTextFieldTintColor = YES;//控制键盘上的工具条文字颜色是否用户自定义
-    manager.enableAutoToolbar = NO;//控制是否显示键盘上的工具条。
+
+    /*
+     IQKeyboardManager *manager = [IQKeyboardManager sharedManager];
+     manager.enable = YES;//控制整个功能是否启用。
+     manager.shouldResignOnTouchOutside = YES;//控制点击背景是否收起键盘
+     manager.shouldToolbarUsesTextFieldTintColor = YES;//控制键盘上的工具条文字颜色是否用户自定义
+     manager.enableAutoToolbar = NO;//控制是否显示键盘上的工具条。
+     */
     
+    [IQKeyboardManager sharedManager].enable = YES;
+
 }
 
 #pragma mark - initialize
