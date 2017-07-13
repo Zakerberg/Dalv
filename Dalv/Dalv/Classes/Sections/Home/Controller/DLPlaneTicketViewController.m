a5aae07beccaa7e423630b456a20d182bc05a0e8
Zakerberg <blmichael1129@icloud.com>
2017-07-13 10:54:37 +0800

    简单搭建机票查询界面

--- Dalv/Dalv/Classes/Sections/Home/Controller/DLPlaneTicketViewController.m
+++ Dalv/Dalv/Classes/Sections/Home/Controller/DLPlaneTicketViewController.m
@@ -4,36 +4,357 @@
 //
 //  Created by Nie on 2017/7/5.
 //  Copyright © 2017年 Michael 柏. All rights reserved.
-//
+//  ------------------- 首页机票 ----------------------
 
 #import "DLPlaneTicketViewController.h"
-#import "TLCityPickerController.h"
 #import "DLPlaneTicketListViewController.h"
-@interface DLPlaneTicketViewController () <TLCityPickerDelegate>
-@property (strong, nonatomic) UIButton *choiceComeCitynBtn;//出发城市
-@property (strong, nonatomic) UIButton *choiceBackCitynBtn;//目的地城市
-@property (strong, nonatomic) UIButton *searchBtn;//搜索
+#import "TLCityPickerController.h"
 
+@interface DLPlaneTicketViewController () <TLCityPickerDelegate,UITableViewDelegate,UITableViewDataSource>
+@property (weak,nonatomic) UITableViewCell* personCell;
+@property(nonatomic,strong) UITableView * planeTicketTableView;
+@property(nonatomic,strong) UILabel * starLabel;/// 出发城市
+@property(nonatomic,strong) UILabel * destinationLabel;/// 目的地城市
+@property (strong, nonatomic) UIButton *searchBtn;/// 搜索
 @end
 
+static NSString *cellID = @"cellID";
 @implementation DLPlaneTicketViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupNavbar];
-    [self setupSubviews];
-    
-    self.view.backgroundColor = [UIColor whiteColor];
+    [self setTab];
 }
 
-#pragma mark - Setup navbar
 - (BOOL)dl_blueNavbar {
     return YES;
 }
-- (void)setupNavbar {
-    self.title = @"机票";
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
 }
 
+- (void)setupNavbar {
+    
+    self.title = @"机票查询";
+    self.view.backgroundColor = [UIColor colorWithHexString:@"#f2f2f2"];
+}
+
+-(void)setTab{
+    
+    self.planeTicketTableView = [[UITableView alloc]initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+    self.planeTicketTableView.dataSource = self;
+    self.planeTicketTableView.delegate = self;
+    self.planeTicketTableView.tableFooterView = [UIView new];
+    self.planeTicketTableView.backgroundColor = [UIColor ms_backgroundColor];
+    
+    [self.view addSubview:self.planeTicketTableView];
+    
+    UIButton *searchBtn = [[UIButton alloc] init];
+    self.searchBtn = searchBtn;
+    [searchBtn setTitle:@"开始查询" forState:UIControlStateNormal];
+    [searchBtn addTarget:self action:@selector(searchBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    searchBtn.backgroundColor = [UIColor colorWithHexString:@"#4d67ee"];
+    searchBtn.layer.cornerRadius = 8.0;
+
+    [self.view addSubview:searchBtn];
+    
+    [self.planeTicketTableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.offset(0);
+    }];
+    
+    [searchBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.planeTicketTableView.tableFooterView.mas_top).offset(43);
+        make.height.offset(44);
+        make.left.equalTo(@15);
+        make.right.equalTo(self.view.mas_right).offset(-15);
+    }];
+}
+
+-(void)searchBtnClick {
+    
+    NSLog(@"开始查询!");
+    
+}
+
+
+#pragma mark ----- UITableView Delegate
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 44;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 3;
+    
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    
+    if (section == 0) {
+        return 0.1;
+    }
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    if (cell == nil) {
+        
+        cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleValue1
+                                      reuseIdentifier: cellID];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    
+    if(indexPath.row == 0) {
+        
+        cell.textLabel.text = @"出发地";
+        cell.textLabel.font = [UIFont systemFontOfSize:15];
+        cell.textColor = [UIColor colorWithHexString:@"#3b3b3b"];
+        
+        UILabel *starLabel = [[UILabel alloc] init];
+        self.starLabel = starLabel;
+        starLabel.font = [UIFont systemFontOfSize:15];
+        starLabel.textColor = [UIColor colorWithHexString:@"#b6b6b6"];
+        cell.detailTextLabel.text = @"选择出发城市";
+        //cell.detailTextLabel.text = self.starLabel.text;
+        
+        [cell.contentView addSubview:starLabel];
+        
+        [starLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.right.offset(-15);
+            make.height.offset(44);
+            make.top.offset(0);
+        }];
+        
+        return cell;
+        
+    }else if (indexPath.row == 1) {
+        
+        cell.textLabel.text = @"目的地";
+        cell.textLabel.font = [UIFont systemFontOfSize:15];
+        cell.textColor = [UIColor colorWithHexString:@"#3b3b3b"];
+        
+        UILabel *starLabel = [[UILabel alloc] init];
+        self.starLabel = starLabel;
+        starLabel.font = [UIFont systemFontOfSize:15];
+        starLabel.textColor = [UIColor colorWithHexString:@"#b6b6b6"];
+        cell.detailTextLabel.text = @"选择目的地";
+        //cell.detailTextLabel.text = self.starLabel.text;
+        
+        [cell.contentView addSubview:starLabel];
+        
+        [starLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.right.offset(-15);
+            make.height.offset(44);
+            make.top.offset(0);
+        }];
+        
+        return cell;
+        
+    }else {
+        
+        cell.textLabel.text = @"出发日期";
+        cell.textLabel.font = [UIFont systemFontOfSize:15];
+        cell.textColor = [UIColor colorWithHexString:@"#3b3b3b"];
+        
+        UILabel *starLabel = [[UILabel alloc] init];
+        self.starLabel = starLabel;
+        starLabel.font = [UIFont systemFontOfSize:15];
+        starLabel.textColor = [UIColor colorWithHexString:@"#b6b6b6"];
+        cell.detailTextLabel.text = @"选择日期";
+        //cell.detailTextLabel.text = self.starLabel.text;
+        
+        [cell.contentView addSubview:starLabel];
+        
+        [starLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.right.offset(-15);
+            make.height.offset(44);
+            make.top.offset(0);
+        }];
+        
+        return cell;
+    }
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    if (indexPath.row == 0){
+        
+    }else if (indexPath.row == 1){
+    
+    
+    }else{
+        
+        
+    }
+}
+
+#pragma mark ----- TLCityPickerDelegate
+
+- (void) cityPickerController:(TLCityPickerController *)cityPickerViewController didSelectCity:(TLCity *)city
+{
+  
+}
+
+- (void) cityPickerControllerDidCancel:(TLCityPickerController *)cityPickerViewController
+{
+    [cityPickerViewController dismissViewControllerAnimated:YES completion:^{
+        
+    }];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 #pragma mark - Setup subViews
 
 - (void)setupSubviews {
@@ -109,5 +430,5 @@
         
     }];
 }
-
+*/
 @end
